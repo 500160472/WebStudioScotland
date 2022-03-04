d7721cb037fe0d9d90b4e3c1e2228d09cee4aab3
500160472 <79994290+500160472@users.noreply.github.com>
2022-03-04 22:15:09 +0000

    set up component structure and text string file

---
+++ Lomond-2022/mothership/src/constants/text.js
@@ -0,0 +1,7 @@
+module.exports = {
+    COMPANY_NAME : 'Lomond Improvements',
+    LOGIN_TITLE : 'Another string',
+    LOGIN_BUTTON : 'tap to login',
+    LOGIN_INPUT_ONE : 'Username...',
+    LOGIN_INPUT_TWO : 'Password...',
+}
--- Lomond-2022/mothership/src/screens/Roof/CompanyOverview.js
+++ Lomond-2022/mothership/src/screens/Roof/CompanyOverview.js
@@ -7,9 +7,10 @@ import C from '../../constants/colours';
 // ASSETS
 import HeaderImage from '../../assets/images/header.png';
 import Accreditations from '../../assets/images/accreditations.png';
+import VideoContainer from '../../components/VideoContainer';
 export default function CompanyOverview(props){
-  const video = React.useRef(null);
-  const [status, setStatus] = React.useState({});
+  // const video = React.useRef(null);
+  // const [status, setStatus] = React.useState({});
     
     return (
   <SafeAreaView>
@@ -24,7 +25,8 @@ export default function CompanyOverview(props){
             <Text style={S.boldTitleDark}>
             At Lomond we are one of the largest roofing and rough casting suppliers to Scotland’s domestic homeowners. Our aim as a business is to provide a world-class service with a local community feel. </Text><Text style={S.subTitleDark}>Our commitment to 'standard working practices' and only using market leading products has caught the attention of the likes of Propensio finance and the National Federation of roofing contractors who are both now one of our trusted partners. Propensio are also the company who provide monthly payment plans for the products and underwrite the guarantee that come with them. Our other certifications include Marley Eternit Roofing, NFRC registered, ISO:9001, ISO:14001, and OHSAS:18001.
             </Text>
-            <View style={S.boxedContainerHighlight}>
+            <VideoContainer url={"http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4"} />
+            {/* <View style={S.boxedContainerHighlight}>
               <Video
                 ref={video}
                 style={S.video}
@@ -41,7 +43,7 @@ export default function CompanyOverview(props){
                   }>
                 <Text style={S.boldSubTitle}>{status.isPlaying ? 'Pause' : 'Play'}</Text>
               </TouchableOpacity>
-            </View>
+            </View> */}
           </ScrollView>
         </View>
         {/* FOOTER  */}
