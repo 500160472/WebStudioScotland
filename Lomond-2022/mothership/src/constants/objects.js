8e686b0ab8b7677339b35212abcf1277c38f70d2
500160472 <79994290+500160472@users.noreply.github.com>
2022-03-14 18:22:31 +0000

    updated online

---
+++ Lomond-2022/mothership/src/constants/objects.js
@@ -0,0 +1,14 @@
+export default {
+    IMAGES: [
+        {
+            id: '1',
+            name: 'test',
+            filePath: 'test.png',
+        },
+        {
+            id: '2',
+            name: 'header image',
+            filePath: 'header.png',
+        },
+    ],
+}; 
--- Lomond-2022/mothership/src/screens/Roof/CompanyOverview.js
+++ Lomond-2022/mothership/src/screens/Roof/CompanyOverview.js
@@ -4,9 +4,10 @@ import { Video, AVPlaybackStatus } from 'expo-av';
 // THEME
 import S from '../../constants/styles';
 import C from '../../constants/colours';
+import O from '../../constants/objects';
 import T from '../../constants/text';
 // ASSETS
-import HeaderImage from '../../assets/images/header.png';
+// import HeaderImage from '../../assets/images/header.png';
 import Accreditations from '../../assets/images/accreditations.png';
 import VideoContainer from '../../components/VideoContainer';
 import Footer from '../../components/Footer';
@@ -22,12 +23,17 @@ export default function CompanyOverview(props){
 
         <View style={S.mainContainerLight}>
           <ScrollView>
+            
             <Image source={HeaderImage} style={S.headerImage} />
             <Image source={Accreditations} style={S.imageFullWidth} />
+            
             <Text style={S.boldTitleDark}>
             At Lomond we are one of the largest roofing and rough casting suppliers to Scotland’s domestic homeowners. Our aim as a business is to provide a world-class service with a local community feel. </Text><Text style={S.subTitleDark}>Our commitment to 'standard working practices' and only using market leading products has caught the attention of the likes of Propensio finance and the National Federation of roofing contractors who are both now one of our trusted partners. Propensio are also the company who provide monthly payment plans for the products and underwrite the guarantee that come with them. Our other certifications include Marley Eternit Roofing, NFRC registered, ISO:9001, ISO:14001, and OHSAS:18001.
             </Text>
+            
             <VideoContainer url={"http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4"} />
+          
+          
           </ScrollView>
         </View>
 
