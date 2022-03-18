11ab2f8e74de6ee2795bbd04d8274a1e47fdc2dc
joshua <joshua@iopewq.uk>
2022-03-18 16:36:52 +0000

    Removed Lomond

--- Lomond-2022/mothership/src/constants/c2--Colours.js
+++
@@ -1,9 +0,0 @@
-export default {
-        white: '#FDFDFF',
-        highlight: '#C7DBFF',
-        lightBlue: '#07A8FE',
-        lomondBlue: '#281670',
-        dark: '#0C002E',
-        gray: '#2D3840',
-  };
-
