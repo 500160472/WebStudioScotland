754e00d5d9131ee0e97e4366349f40dfc71e63be
joshua <joshua@iopewq.uk>
2022-03-19 13:55:40 +0000

    tweaks before reset

--- reactnativeradio-master/App.js
+++ reactnativeradio-master/App.js
@@ -6,6 +6,12 @@ import TrackPlayer from 'react-native-track-player';
 
 import App from './src/App';
 import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
-TrackPlayer.registerPlaybackService(() => require('./service'));
+function MainApp() {
+            
+    TrackPlayer.registerPlaybackService(() => require('./service'));
+    <App></App>
+;
+}
+    export default App;
+    
+// React.registerComponent(appName, () => App);
